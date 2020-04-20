6045b456c0c3bb2ede7e58db3152120457754300
GagahGK <gagah.ghaniswara51@hotmail.com>
2020-04-20 17:05:09 +0700

    Add Rules

--- inferensi.py
+++ inferensi.py
@@ -2,20 +2,54 @@ class Inferensi :
     
     x = {} #Rules Array Dictionary
     #Maks 1 karena hanya ada label untuk tinggi dan rendah
+    #[TPA_Label][UN_Label][Jarak_label]
+    #TPA    : 0 = Tinggi , 1 = Sedang , 2 = Rendah
+    #UN     : 0 = A , 1 = B , 2 = C, 3 =D
+    #Jarak  : 0 = Dekat, 1 = Normal, 2 = Jauh
     #Tinggi
     x[0][0][0] = 1
-    x[0][0][0] = 1
-    x[0][0][0] = 1
-    x[0][0][0] = 1
-    x[0][0][0] = 1
-    x[0][0][0] = 1
-    #Rendah
-    x[0][0][0] = 0
-    x[0][0][0] = 0
-    x[0][0][0] = 0
-    x[0][0][0] = 0
-    x[0][0][0] = 0
-
+    x[0][1][0] = 1
+    x[0][2][0] = 1
+    x[0][3][0] = 1
+    x[1][0][0] = 1
+    x[1][1][0] = 1
+    x[1][2][0] = 1
+    x[1][3][0] = 1
+    x[2][0][0] = 1
+    x[2][1][0] = 1
+    x[2][2][0] = 1
+    x[2][3][0] = 1
+    #Tinggi (UN = C|D)
+    x[0][0][1] = 1
+    x[0][1][1] = 1
+    x[1][0][1] = 1
+    x[1][1][1] = 1
+    x[2][0][1] = 1
+    x[2][1][1] = 1
+    #[TPA_Label][UN_Label][Jarak_label]
+    #TPA    : 0 = Tinggi , 1 = Sedang , 2 = Rendah
+    #UN     : 0 = A , 1 = B , 2 = C, 3 =D
+    #Jarak  : 0 = Dekat, 1 = Normal, 2 = Jauh
+    #Rendah (Jauh)
+    x[0][0][2] = 0
+    x[0][1][2] = 0
+    x[0][2][2] = 0
+    x[0][3][2] = 0
+    x[1][0][2] = 0
+    x[1][1][2] = 0
+    x[1][2][2] = 0
+    x[1][3][2] = 0
+    x[2][0][2] = 0
+    x[2][1][2] = 0
+    x[2][2][2] = 0
+    x[2][3][2] = 0
+    #Rendah (UN C|D)
+    x[0][2][1] = 0
+    x[0][3][1] = 0
+    x[1][2][1] = 0
+    x[1][3][1] = 0
+    x[2][2][1] = 0
+    x[3][3][1] = 0
 
     def __init__(self, TPA, UN, Jarak):
         #Ekstrak Array
@@ -36,7 +70,7 @@ class Inferensi :
                             if Jarak <= 0 :
                                 pass
                             else : 
-                                #Get Value of NK not which NK
+                                #Get "Value" of NK not "which" NK
                                 val = min(tpa,un,jarak)
                                 NKlabel = x[i][j][k]
                                 if (NKlabel == 1):
