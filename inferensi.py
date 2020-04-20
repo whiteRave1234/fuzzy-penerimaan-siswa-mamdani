6dffb01c28208637a740b82ef4f5bde719795e7e
GagahGK <gagah.ghaniswara51@hotmail.com>
2020-04-20 16:40:00 +0700

    add description to inferensi class

--- inferensi.py
+++ inferensi.py
@@ -1,8 +1,8 @@
 class Inferensi :
     
-    x = {}
+    x = {} #Rules Array Dictionary
     #Maks 1 karena hanya ada label untuk tinggi dan rendah
-    #TInggi
+    #Tinggi
     x[0][0][0] = 1
     x[0][0][0] = 1
     x[0][0][0] = 1
@@ -22,14 +22,17 @@ class Inferensi :
         # y = []
         # z = []
         for i,tpa in enumerate(TPA):
+            #i maks 3
             if tpa <= 0 : 
                 pass
             else : 
                 for j,un in enumerate(UN):
+                    #j maks 3
                     if UN <= 0 :
                         pass
                     else : 
                         for k,jarak in enumerate(Jarak):
+                            #k maks 4
                             if Jarak <= 0 :
                                 pass
                             else : 
