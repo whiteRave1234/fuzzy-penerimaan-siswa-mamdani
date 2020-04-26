2666b552ac61ecdaeda069cf423da16e97dbcfc9
GagahGK <gagah.ghaniswara51@hotmail.com>
2020-04-26 15:45:04 +0700

    More Labelling

--- FuzzyLogic.py
+++ FuzzyLogic.py
@@ -2,11 +2,6 @@ from Fuzzifikasi import Fuzzifikasi as Fuzzy
 from inferensi import Inferensi as Infer
 from Defuzifikasi import Defuzifkasi as Defuz
 
-# x=0;    a=0
-# b=0;    c=0
-# adenum=0;    bdenum=0
-# cdenum=0;    numerator=0
-
 
 enggar=Fuzzy("Enggar")
 wayan=Fuzzy("Wayan")
